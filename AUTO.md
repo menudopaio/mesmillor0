35f6bf7fe91acc3cb6ce7e1c793a3c3ed9cb8786
menudopaio <dgffunde21@gmail.com>
2024-03-26 16:41:24 +0100

    First commit

---
+++ AUTO.md
@@ -0,0 +1,17 @@
+# IR APLICANDO (ideas claras)
+
+1. Dar funcion a botones idiomas
+2. Yoga postnatal html
+3. Yoga para embarazadas html
+4. Clases de Yoga (LINKS A embarazadas y postnatal)
+5. Psiconutricion br bonitos
+6. Responsive
+7. Barra busqueda intraweb
+
+
+
+# PREGUNTAR A ANA
+- Quieres que ponga barcelona o da igual (o es cuestion de seo)? "/clases-de-yoga-barcelona" o "/clases-de-yoga" links
+- Home -> Saber Mas (404 NOT FOUND)
+- Psicologia -> TARIFAS (404 NOT FOUND)
+- Yoga postnatal -> Saber mas (404 NOT FOUND)
---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+# Don't update automatically each refresh
+app.config['SESSION_REFRESH_EACH_REQUEST'] = False
+
+# Don't store in cache
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+Session(app)
+
+# If changes here, change also layout.html
+HEADERiTEMS = [
+    "Home",
+    "Clases de Yoga",
+    "Psiconutrición",
+    "Psicologia"
+]
+
+# HOME PAGE
+@app.route("/")
+def home():
+    return render_template ("home.html")
+
+# PSICONUTRICION
+@app.route("/psiconutricion")
+def psiconutricion():
+    return render_template("psiconutricion.html")
+
+# PSICOLOGIA
+@app.route("/psicologia")
+def psicologia():
+    return render_template("psicologia.html")
+
+
+# Clases de Yoga
+@app.route("/clases-de-yoga")
+def clasesYoga():
+    return render_template("clasesYoga.html")
+
+# Yoga para embarazadas
+@app.route("/yoga-para-embarazadas")
+def yogaEmbarazadas():
+    return render_template("yogaEmbarazadas.html")
+
+# Yoga infantil
+@app.route("/yoga-infantil")
+def yogaInfantil():
+    return render_template("yogaInfantil.html")
+
+# Yoga postnatal
+@app.route("/yoga-postnatal")
+def yogaPostnatal():
+    return render_template("yogaPostnatal.html")
+
+# 404 NOT FOUND Entrena tu mente cuida tu cuerpo
+@app.route("/entrena-tu-mente-cuida-tu-cuerpo")
+def entrenaMenteCuidaCuerpo():
+    return render_template ("entrenaCuida.html")
+
+# Lo que nos importa
+@app.route("/sobre-mes-millor")
+def sobreMesMillor():
+    return render_template ("sobreMesMillor.html")
